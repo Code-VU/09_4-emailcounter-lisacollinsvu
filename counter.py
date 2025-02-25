b693759894a67378f1064b8890887ae81e9b44a9
Lisa Collins <Lisa.Collins@veteransunited.com>
2025-02-25 10:17:01 -0600

    LisaCollinsEmailCounter

--- counter.py
+++ counter.py
@@ -1,9 +1,23 @@
 def countEmail():
     # This first line is provided for you
+    counter = {}
+    max = 0
+    email = None
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
-        
+    for line in handle:
+        if line.startswith("From:"):
+            line = line.split()
+            try:
+                if line[1] not in counter: counter[line[1]] = 1
+                else: counter[line[1]] = counter[line[1]] + 1
+            except: next
+    for key in counter:
+        if max == 0 or counter[key] > max: 
+            max = counter[key]
+            email = key
+    print(email, max)
 
 ## if you want to test locally run > python counter.py
 if __name__ == "__main__":
